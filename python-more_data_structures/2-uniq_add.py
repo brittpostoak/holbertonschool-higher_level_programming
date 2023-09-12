dc8d9265d64e6cac974f495f24ff956d29fc6d4a
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 16:51:37 -0700

    task 2 edit

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    seen = set()
-  sum = 0
-  for num in my_list:
-    if num not in seen:
-      seen.add(num)
-      sum += num
-  return sum
+
+    new_list = set(my_list)
+    return sum(new_list)
