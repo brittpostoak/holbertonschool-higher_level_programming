aad17c7cf4656c2bd5587645c23a2e9a3dcfb933
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 16:33:24 -0700

    task 0 1 edit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
+
+        new_matrix = matrix.copy()
+    length = len(new_matrix)
+
+    for i in range(length):
+        new_matrix[i] = list(map(lambda x: x * x, new_matrix[i]))
+
+    return(new_matrix)
