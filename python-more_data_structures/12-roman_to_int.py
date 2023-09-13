f6678ebeef33d1a147e7b8558c8618d244fc2af7
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 20:10:53 -0700

    task 12 edit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,30 +3,10 @@
 def roman_to_int(roman_string):
 
     if not roman_string or not isinstance(roman_string, str):
-    return 0
-
-  roman_numeral_dict = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000,
-  }
-
-  total = 0
-  i = 0
-  while i < len(roman_string):
-    current_value = roman_numeral_dict[roman_string[i]]
-    next_value = None
-    if i + 1 < len(roman_string):
-      next_value = roman_numeral_dict[roman_string[i + 1]]
-
-    if next_value and current_value < next_value:
-      total -= current_value
-    else:
-      total += current_value
-    i += 1
-
-  return total
+        return (0)
+    Roma = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roma = roman_string
+    ilist = [Roma[i[0]] if Roma[i[0]] >= Roma[i[1]] else (-1*Roma[i[0]])
+             for i in zip(roma, roma[1:] + roma[-1])]
+    dec = sum(ilist)
+    return (dec)
