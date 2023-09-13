c6d366adfa07a45b69c75ddc9f82504c19167314
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 20:17:19 -0700

    task 2-5

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys  # access to command line args
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")  # no arguments passed, only program name
+    elif len(sys.argv) == 2:
+        print("1 argument:")  # one argument passed
+        print("1: {}".format(sys.argv[1]))  # print single argument
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))  # more than one args
+        for x in range(1, len(sys.argv)):
+            print("{}: {}".format(x, sys.argv[x]))  # print each argument
