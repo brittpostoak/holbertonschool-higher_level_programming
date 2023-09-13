36df874d731217901a699f87f715f7ccfd726e32
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 18:02:09 -0700

    task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    # Multiply elements one by one
-    result = 1
-    for x in myList:
-        result = result * x
-    return result
 
+    y=[]
+    for i in my_list:
+        y.append(i*multiplier)
+    return y
 
-# Driver code
-list1 = [1, 2, 3]
-list2 = [3, 2, 4]
-print(multiplyList(list1))
-print(multiplyList(list2))
+print(multiply_list_by(my_list,10))
