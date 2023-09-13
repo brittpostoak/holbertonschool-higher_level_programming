2bc74d9f9a1754d67756c8c6014e86b55ad3e0a4
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 17:51:28 -0700

    task 10-11 edit syntax

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,9 +2,7 @@
 
 def best_score(a_dictionary):
 
-    def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
     else:
-        max_key = max(a_dictionary, key=a_dictionary.get)
-        return max_key
+        return str(max(a_dictionary, key=a_dictionary.get))
