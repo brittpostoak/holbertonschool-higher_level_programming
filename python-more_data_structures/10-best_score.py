26c3f6dc450ee7bb49c889fa71b99cbace854ac7
Brittany_Herman <brittherman86@gmail.com>
2023-09-12 17:37:21 -0700

    task 4-12 edit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+
 def best_score(a_dictionary):
+
+    def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    else:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python3
+
 def roman_to_int(roman_string):
+
+    if not roman_string or not isinstance(roman_string, str):
+    return 0
+
+  roman_numeral_dict = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000,
+  }
+
+  total = 0
+  i = 0
+  while i < len(roman_string):
+    current_value = roman_numeral_dict[roman_string[i]]
+    next_value = None
+    if i + 1 < len(roman_string):
+      next_value = roman_numeral_dict[roman_string[i + 1]]
+
+    if next_value and current_value < next_value:
+      total -= current_value
+    else:
+      total += current_value
+    i += 1
+
+  return total
