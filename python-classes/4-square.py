91a0710f77bade7521881274b5cb68d24746b0f3
Brittany_Herman <brittherman86@gmail.com>
2023-09-14 21:17:07 -0700

    task 0-6

---
+++ python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Square():
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, x):
+        if not isinstance(x, int):
+            raise TypeError("size must be an integer")
+        if x < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = x
+
+    def area(self):
+        return (self.__size * self.__size)
---
+++ python-classes/6-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Square():
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, x):
+        if not str(x).isdigit():
+            raise TypeError("size must be an integer")
+        if x < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = x
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
