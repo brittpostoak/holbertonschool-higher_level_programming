0abdada83cb18114c665e6aa49b49c9cc5d4e3f9
Brittany_Herman <brittherman86@gmail.com>
2023-09-19 21:04:52 -0700

    task 0 edit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,14 +17,20 @@ def add_integer(a, b=98):
     Returns:
          Addition of two numbers
     """
+     if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    
+
     elif type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    
+
     else:
         a = int(a)
         b = int(b)
-    
+
     return a + b
