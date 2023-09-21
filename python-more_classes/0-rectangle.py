63a5e654005c70f2008ca0f2f1b41d873e7f3464
Brittany_Herman <brittherman86@gmail.com>
2023-09-20 21:08:17 -0700

    task 0 syntax

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-"""
-Class that defines a rectangle.
-"""
+""" Class that defines a rectangle. """
 
 
 class Rectangle:
-    """Rectangle class."""
-
-    def __init__(self):
-        """Initialize class."""
-        pass
+    """ Defines the rectangle. """
+    pass
