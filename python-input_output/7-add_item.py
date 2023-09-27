46aaebd171f6baf2ac54bc8b37a63845b9cec91c
Brittany_Herman <brittherman86@gmail.com>
2023-09-26 21:36:10 -0700

    task 7 edit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,18 @@
 """
 Script that adds all arguments to list then saves them to file.
 """
-import json
 
 
-def save_to_json_file(my_obj, filename):
-    """
-    Begins function - Save to json.
-    """
-    with open(filename, encoding='utf-8', mode='w') as f:
-        f.write(json.dumps(my_obj))
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    arglist = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arglist = []
+
+arglist += argv[1:]
+save_to_json_file(arglist, "add_item.json")
