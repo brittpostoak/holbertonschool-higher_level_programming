0827b5e4ab5c93defb6d6b913621d7a092413451
Brittany_Herman <brittherman86@gmail.com>
2023-09-26 21:12:49 -0700

    task 4 edit

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,9 +4,7 @@ Function that returns object represented by a JSON string.
 """
 
 
-def append_write(filename="", text=""):
-    """
-    Begins function - append_write.
-    """
-    with open(filename, encoding='utf-8', mode='a') as f:
-        return f.write(text)
+def from_json_string(my_str):
+    """Begins function - from json string"""
+    import json
+    return json.loads(my_str)
