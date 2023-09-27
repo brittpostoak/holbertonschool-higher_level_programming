8fd169cf0b1021e646c079e44ca7d9c2b5517add
Brittany_Herman <brittherman86@gmail.com>
2023-09-26 21:47:32 -0700

    task 10 edit

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,10 +4,23 @@ Class Student that defines a student by age, first and last name.
 """
 
 
-def class_to_json(obj):
-    """
-    Begins function - class to json
-    """
+class Student():
+    """Class to define Student."""
 
-    if hasattr(obj, '__dict__'):
-        return obj.__dict__
+    def __init__(self, first_name, last_name, age):
+        """Begins function - initialize instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Begins function - to json."""
+        if type(attrs) is list:
+            new_dict = {}
+            for key in self.__dict__:
+                for key2 in attrs:
+                    if key == key2:
+                        new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
