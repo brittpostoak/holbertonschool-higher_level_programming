0672190685ef99de71221f607c409918075ecd3d
Brittany_Herman <brittherman86@gmail.com>
2023-09-26 21:02:31 -0700

    task 2 edit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,15 +5,7 @@ of characters.
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """
-    Begins function - read_lines.
-    """
-    n = []
-    with open(filename, "r") as f:
-        for index, line in enumerate(f):
-            if index < nb_lines or nb_lines == 0:
-                l.append(line)
-            else:
-                break
-        print("{}".format("".join(n)), end="")
+def append_write(filename="", text=""):
+    """Begins function - append write."""
+    with open(filename, 'a') as f:
+        return f.write(text)
