530a4863cf047e8dfe4362f0ba593c62609f0f4f
Your Name <you@example.com>
2023-10-24 21:52:17 -0700

    task 10-14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,15 @@
-i
+#!/usr/bin/python3
+""" Class: City """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ Class City, database location """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
