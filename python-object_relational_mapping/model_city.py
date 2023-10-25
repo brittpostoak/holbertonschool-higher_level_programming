b127b4c112367624ee33b6a33fb688c6dc128acd
Your Name <you@example.com>
2023-10-24 22:00:40 -0700

    edit

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" Class: City """
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+# edit
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from model_city import City
 
 Base = declarative_base()
 
 
-class City(Base):
-    """ Class City, database location """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement="auto", unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
